932f85ab49c37b93b5a4f9b7ca6545e48aa6bfcc
Joe <skybluejoe123@gmail.com>
2020-11-11 22:30:02 +0800

    init commit

---
+++ assets/inc/navbar.php
@@ -0,0 +1,22 @@
+<nav class="navbar">
+    <a class="logo" href="#">Lorem ipsum dolor</a>
+    <label class="menu-btn" for="menu-toggle"><i class="fas fa-bars"></i></label>
+    <input type="checkbox" id="menu-toggle">
+    <ul>
+        <li>
+            <a href="#">section1</a>
+        </li>
+        <li>
+            <a href="#">section2</a>
+        </li>
+        <li>
+            <a href="#">section3</a>
+        </li>
+        <li>
+            <a href="#">section4</a>
+        </li>
+        <li>
+            <a href="#">section5</a>
+        </li>
+    </ul>
+</nav>
---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>php practice</title>
+    <link rel="stylesheet" href="./assets/stylesheets/all.css">
+    <script src="https://kit.fontawesome.com/b22ce6914a.js" crossorigin="anonymous"></script>
+</head>
+<body>
+<?php include('./assets/inc/navbar.php') ?>
+</body>
+</html>
